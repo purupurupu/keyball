0a78108e2c6e701eb74ab829591418c87c6e9f22
purupurupu <purupurupu0prog@gmail.com>
2025-05-08 20:24:22 +0900

    feat:add combo term

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -77,3 +77,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+// For combo
+#define COMBO_TERM 80
