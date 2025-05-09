b728e5ce771cc7ab491c69cc73a1d71ae8a4b6ed
purupurupu <purupurupu0prog@gmail.com>
2025-05-09 20:57:04 +0900

    wip

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via_custom/keymap.c
@@ -72,8 +72,8 @@ void oledkit_render_info_user(void) {
 
 // Custom combo
 #ifdef COMBO_ENABLE
-const uint16_t PROGMEM my_comma[] = {LT(3, KC_M), KC_BTN1, COMBO_END};
-const uint16_t PROGMEM my_dot[] = {KC_BTN1, KC_BTN2, COMBO_END};
+const uint16_t PROGMEM my_comma[] = {K_C, KC_BTN1, COMBO_END};
+const uint16_t PROGMEM my_dot[] = {KC_X, KC_BTN2, COMBO_END};
 
 combo_t key_combos[] = {
   COMBO(my_comma, KC_COMMA),
